9892d4e5fb7fab0da71b86cd3279121ae9d7fad7
NicCaterino <caterino.2054849@studenti.uniroma1.it>
2024-10-24 18:07:12 +0200

    ros_build_run

--- README.md
+++ README.md
@@ -5,10 +5,34 @@
 # How to compile
 
 ```code
+./ros_build_run --c
+'''
+
+or 
+
+```code
 cd ./rp_ws
 source /opt/ros/noetic/setup.bash
 catkin_make
 source /deve/setup.bash
+```
+
+# How to execute
+start roscore in another terminal than
+
+```code
+./ros_build_run --e
+'''
+
+or alternatevely
+
+
+```code
+# In a separate instace of the terminal
+source /opt/ros/noetic/setup.bash
+roscore
+# In the terminal window used to run the code
+rosrun mrsim mrsim_node cappero_1r.json
 ```
 
 
--- rp_ws/src/mrsim/bin/mrsim_node.cpp
+++ rp_ws/src/mrsim/bin/mrsim_node.cpp
@@ -12,11 +12,32 @@
 
 
 int main(int argc, char** argv) {
+    if (argc < 2) {
+    cerr << "Error: no config.jsosn file provided:\n" 
+    << "Please insert your configuration file in the project config directory.\n"
+    << "Then pass the name of the file as arg to the node.\n" << endl;
+    return 1;
+  }
+
+
+  
   ros::init(argc, argv, "mrsim_node");
   ros::NodeHandle nh("/");
-
+  string git_root_path;
+  try {
+    git_root_path = getGitRootPath();
+  }
+  catch (const runtime_error& e) {
+    cerr << "Exception: " << e.what() << '\n';
+  }
   // Load the configuration file and initialize the simulator
-
+    // Load configuration file
+  string config_path = git_root_path + "/config/" + argv[1];
+  Json::Value root = readJson(config_path);
+  string map = root["map"].asString();
+  cout << "Map -> " << map << endl;
+  string image_path = git_root_path + "/map/" +  map;
+  
   while (ros::ok()) {
     // run a simulation iteration
     // visualize the simulation
